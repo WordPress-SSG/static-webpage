3299d82b6bb0da3c595aaa1ae9f2c269cc810b52
Hào Nghiêm <qtvhao@gmail.com>
2025-03-03 23:55:02 +0700

    Update fetch_website.sh

--- fetch_website.sh
+++ fetch_website.sh
@@ -2,7 +2,7 @@
 
 echo "Starting website fetch for $DOMAIN"
 
-wget -q --no-check-certificate --reject-regex="/wp-content/uploads/|add-to-cart|/wp-json/" -nc \
+wget -vv --no-check-certificate --reject-regex="/wp-content/uploads/|add-to-cart|/wp-json/" -nc \
  --user-agent="$USERAGENT" \
  --recursive --page-requisites --adjust-extension --span-hosts \
  --domains "$DOMAIN" --restrict-file-names=unix --convert-links --max-redirect 5 "https://$DOMAIN"
