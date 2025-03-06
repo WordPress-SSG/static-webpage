c3eb26b9ba9873045d17b80cc0bcc00c31867114
Hào Nghiêm <qtvhao@gmail.com>
2025-03-06 23:50:57 +0700

    Update fetch_website.sh

--- fetch_website.sh
+++ fetch_website.sh
@@ -7,4 +7,16 @@ wget -vv --no-check-certificate --reject-regex="/wp-content/uploads/|add-to-cart
  --recursive --page-requisites --adjust-extension --span-hosts \
  --domains "$DOMAIN" --restrict-file-names=unix --convert-links --max-redirect 5 "http://$DOMAIN"
 
+# Source and destination directories
+SRC_DIR="/wp-ssg/plugins"
+DEST_DIR="/data/$DOMAIN/wp-content/plugins"
+
+# Ensure the destination directory exists
+mkdir -p "$DEST_DIR"
+
+# Find and copy .js and .css files while preserving directory structure
+rsync -av --include="*/" --include="*.js" --include="*.css" --exclude="*" "$SRC_DIR/" "$DEST_DIR/"
+
+echo "JavaScript and CSS files have been copied successfully!"
+
 echo "Website fetch completed!"
