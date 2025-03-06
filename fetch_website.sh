c7386ebb10e4c735a96858bd352ddf1450b95938
Hào Nghiêm <qtvhao@gmail.com>
2025-03-07 00:10:17 +0700

    Update fetch_website.sh

--- fetch_website.sh
+++ fetch_website.sh
@@ -7,15 +7,20 @@ wget -vv --no-check-certificate --reject-regex="/wp-content/uploads/|add-to-cart
  --recursive --page-requisites --adjust-extension --span-hosts \
  --domains "$DOMAIN" --restrict-file-names=unix --convert-links --max-redirect 5 "http://$DOMAIN"
 
-# Source and destination directories
-SRC_DIR="/wp-ssg/plugins"
-DEST_DIR="/data/$DOMAIN/wp-content/plugins"
+# Function to sync files
+sync_files() {
+    local SRC_DIR="$1"
+    local DEST_DIR="$2"
+    
+    # Ensure the destination directory exists
+    mkdir -p "$DEST_DIR"
+    
+    rsync -av --include="*/" --include="*.js" --include="*.css" --include="*.jpg" --include="*.png" --include="*.gif" --exclude="*" "$SRC_DIR/" "$DEST_DIR/"
+}
 
-# Ensure the destination directory exists
-mkdir -p "$DEST_DIR"
-
-# Find and copy .js, .css, and image files while preserving directory structure
-rsync -av --include="*/" --include="*.js" --include="*.css" --include="*.jpg" --include="*.png" --include="*.gif" --exclude="*" "$SRC_DIR/" "$DEST_DIR/"
+# Call the function with arguments
+sync_files "/wp-ssg/plugins" "/data/$DOMAIN/wp-content/plugins"
+sync_files "/tmp/contents/wp-content-$DOMAIN-unzipped/var/www/html/wp-content" "/data/$DOMAIN/wp-content/"
 
 echo "JavaScript and CSS files have been copied successfully!"
 
